437416596c36ea34e4ba443caa9363a3f4b38938
joasegovia9427 <joasegovia9427@gmail.com>
2024-01-20 19:12:39 -0300

    Improves

--- resources/js/layout/Navbar/components/index.ts
+++ resources/js/layout/Navbar/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./NavbarLogo";
+export * from "./NavbarLogoSmall";
 export * from "./LogOutLogo";
--- resources/js/screens/forms/components/FormDropdown.tsx
+++ resources/js/screens/forms/components/FormDropdown.tsx
@@ -1,64 +1,67 @@
-import { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
+import React, { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
 import {
   ArchiveBoxIcon,
   ArrowRightCircleIcon,
   DocumentDuplicateIcon,
   HeartIcon,
-  PencilSquareIcon,
   TrashIcon,
   UserPlusIcon,
-} from '@heroicons/react/20/solid'
-import { icons } from '@/ui'
-import type { FormDropdownItem } from '@/shared.types'
+} from "@heroicons/react/20/solid";
+
+import type { FormDropdownItem } from "@/shared.types";
+import { icons } from "@/ui";
 
 interface FormDropdownProps {
   mode: "FORM" | "USERS";
-  items: FormDropdownItem[],
-  param?: string | number,
+  items: FormDropdownItem[];
+  param?: string | number;
 }
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 const buildHref = (mode: string, itemName: string, param?: string | number) => {
   if (mode === "USERS") {
     switch (itemName) {
-      case 'Edit':
-        return `/users/${param}`
-      case 'Duplicate':
-        return `/users/${param}/duplicate`
-      case 'Archive':
-        return `/users/${param}/archive`
-      case 'Move':
-        return `/users/${param}/move`
-      case 'Share':
-        return `/users/${param}/share`
-      case 'Add to favorites':
-        return `/users/${param}/add-to-favorites`
+      case "Edit":
+        return `/users/${param}`;
+      case "Duplicate":
+        return `/users/${param}/duplicate`;
+      case "Archive":
+        return `/users/${param}/archive`;
+      case "Move":
+        return `/users/${param}/move`;
+      case "Share":
+        return `/users/${param}/share`;
+      case "Add to favorites":
+        return `/users/${param}/add-to-favorites`;
     }
-  }
-  else // mode === "FORM"
-  {
+  } // mode === "FORM"
+  else {
     switch (itemName) {
-      case 'Edit':
-        return `/forms/${param}`
-      case 'Duplicate':
-        return `/forms/${param}/duplicate`
+      case "Edit":
+        return `/forms/${param}`;
+      case "Duplicate":
+        return `/forms/${param}/duplicate`;
     }
   }
-}
+};
 
-export const FormDropdown: React.FC<FormDropdownProps> = ({ items, mode, param }) => {
+export const FormDropdown: React.FC<FormDropdownProps> = ({
+  items,
+  mode,
+  param,
+}) => {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex">
+        <Menu.Button className=" inline-flex">
           <icons.ThreeDotsIcon />
+          {/* <icons.EllipsisHorizontalIcon /> */}
         </Menu.Button>
       </div>
-
       <Transition
         as={Fragment}
         enter="transition ease-out duration-100"
@@ -68,8 +71,8 @@ export const FormDropdown: React.FC<FormDropdownProps> = ({ items, mode, param }
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute max-h-[300px] overflow-scroll right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          {items.map((item, idx) => (
+        <Menu.Items className="absolute right-0 z-10 mt-[-15px] max-h-[300px] w-56 origin-top-right divide-y divide-gray-100 overflow-scroll rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          {items.map((item, idx) =>
             item.newSection ?? idx === 0 ? (
               <div key={idx} className="py-1">
                 <Menu.Item key={idx}>
@@ -77,14 +80,15 @@ export const FormDropdown: React.FC<FormDropdownProps> = ({ items, mode, param }
                     <a
                       href={buildHref(mode, item.name, param)}
                       className={classNames(
-                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                        'group flex items-center px-4 py-2 text-sm'
+                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                        "group flex items-center px-4 py-2 text-sm",
                       )}
                     >
-                      <PencilSquareIcon
-                        className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-                        aria-hidden="true"
-                      />
+                      {React.cloneElement(item.icon, {
+                        className:
+                          "mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500",
+                        "aria-hidden": "true",
+                      })}
                       {item.name}
                     </a>
                   )}
@@ -96,19 +100,21 @@ export const FormDropdown: React.FC<FormDropdownProps> = ({ items, mode, param }
                   <a
                     href={buildHref(mode, item.name, param)}
                     className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      'group flex items-center px-4 py-2 text-sm'
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "group flex items-center px-4 py-2 text-sm",
                     )}
                   >
-                    <PencilSquareIcon
-                      className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-                      aria-hidden="true"
-                    />
+                    {React.cloneElement(item.icon, {
+                      className:
+                        "mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500",
+                      "aria-hidden": "true",
+                    })}
                     {item.name}
                   </a>
                 )}
               </Menu.Item>
-            )))}
+            ),
+          )}
           {/* <div className="py-1">
             <Menu.Item>
               {({ active }) => (
@@ -119,7 +125,7 @@ export const FormDropdown: React.FC<FormDropdownProps> = ({ items, mode, param }
                     'group flex items-center px-4 py-2 text-sm'
                   )}
                 >
-                  <PencilSquareIcon
+                  <PencilIcon
                     className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
                     aria-hidden="true"
                   />
@@ -227,7 +233,7 @@ export const FormDropdown: React.FC<FormDropdownProps> = ({ items, mode, param }
         </Menu.Items>
       </Transition>
     </Menu>
-  )
-}
+  );
+};
 
 export default FormDropdown;
