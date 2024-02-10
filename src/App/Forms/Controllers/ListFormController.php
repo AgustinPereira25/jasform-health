7ed09a0621e3f0c8b4d9ec37b7d46af9beb241e0
Sebastian Insausti <sinsausti.uy@gmail.com>
2024-02-09 20:45:18 -0800

    Update ListFormController.php

--- src/App/Forms/Controllers/ListFormController.php
+++ src/App/Forms/Controllers/ListFormController.php
@@ -7,15 +7,30 @@ use Domain\Forms\Models\Form;
 use App\Forms\Transformers\FormTransformer;
 use Illuminate\Http\JsonResponse;
 use Spatie\QueryBuilder\QueryBuilder;
+use Illuminate\Pagination\Paginator;
+
 
 class ListFormController
 {
     public function __invoke(Request $request): JsonResponse
     {
+        $perPage = $request->get('perPage', 1);
+        $currentPage = $request->get('currentPage', 1);
+        $isActive = $request->get('isActive', "false");
+
+        Paginator::currentPageResolver(function () use ($currentPage) {
+            return $currentPage;
+        });
+
         $forms = QueryBuilder::for(Form::class)
             ->withCount('form_instances')
-            ->withCount('form_questions')
-            ->get();
+            ->withCount('form_questions');
+
+            if ($isActive == "true") {
+                $forms->where('is_active', true);
+            };
+
+        $forms = $forms->paginate($perPage);
 
         return responder()
             ->success($forms, FormTransformer::class)
