14420a8036817b28e0355091eb242213ba26692c
joasegovia9427 <joasegovia9427@gmail.com>
2024-02-17 01:34:49 -0300

    Improve code

--- resources/js/ui/common/Skeletons/TableSkeleton.tsx
+++ resources/js/ui/common/Skeletons/TableSkeleton.tsx
@@ -5,11 +5,11 @@ const TableSkeleton = () => {
             className="animate-pulse space-y-4 divide-y divide-gray-300 border border-gray-300 p-4 shadow rounded-lg md:p-6 pb-0 h-[509px]"
         >
             <div className="h-5 flex items-center justify-between pt-4">
-                <div className="h-5 w-56 rounded-full bg-gray-300 "></div>
-                <div className="h-5 w-56 rounded-full bg-gray-300 "></div>
-                <div className="h-5 w-32 rounded-full bg-gray-300 "></div>
-                <div className="h-5 w-32 rounded-full bg-gray-300 "></div>
-                <div className="h-5 w-10 rounded-full bg-white "></div>
+                <div className="h-5 w-56 rounded-full bg-gray-300"></div>
+                <div className="h-5 w-56 rounded-full bg-gray-300"></div>
+                <div className="h-5 w-32 rounded-full bg-gray-300"></div>
+                <div className="h-5 w-32 rounded-full bg-gray-300"></div>
+                <div className="h-5 w-10 rounded-full bg-white"></div>
             </div>
 
             {Array.from({ length: 4 }).map((_, index) => (
@@ -18,19 +18,19 @@ const TableSkeleton = () => {
                     className="flex h-[75px] items-center justify-between pt-4"
                 >
                     <div className="h-5 w-56 rounded-full bg-gray-300"></div>
-                    <div className="h-5 w-56 rounded-full bg-gray-300 "></div>
-                    <div className="h-5 w-32 rounded-full bg-gray-300 "></div>
-                    <div className="h-5 w-32 rounded-full bg-gray-300 "></div>
-                    <div className="h-5 w-10 rounded-full bg-gray-300 "></div>
+                    <div className="h-5 w-56 rounded-full bg-gray-300"></div>
+                    <div className="h-5 w-32 rounded-full bg-gray-300"></div>
+                    <div className="h-5 w-32 rounded-full bg-gray-300"></div>
+                    <div className="h-5 w-10 rounded-full bg-gray-300"></div>
                 </div>
             ))}
 
-            <div className="flex    items-center justify-end border-t border-gray-200 bg-white px-4 pb-3 sm:px-6 pt-8">
+            <div className="flex items-center justify-end border-t border-gray-200 bg-white px-4 pb-3 sm:px-6 pt-8">
                 <div className="1 mr-4 flex items-center justify-between">
-                    <div className="h-5 w-32 rounded-full bg-gray-300 "></div>
+                    <div className="h-5 w-32 rounded-full bg-gray-300"></div>
                 </div>
                 <div className="flex items-center justify-end">
-                    <div className="h-5 w-80 rounded-full bg-gray-300 "></div>
+                    <div className="h-5 w-80 rounded-full bg-gray-300"></div>
                 </div>
             </div>
         </div>
