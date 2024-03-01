b306dd66c2b413b82a8ef30aa562dd60bab869d0
joasegovia9427 <joasegovia9427@gmail.com>
2024-02-29 21:10:16 -0300

    WIP

--- src/App/Forms/Controllers/UpdateFormController.php
+++ src/App/Forms/Controllers/UpdateFormController.php
@@ -8,19 +8,32 @@ use Domain\Forms\Actions\UpdateFormAction;
 use Domain\Forms\Models\Form;
 use Illuminate\Http\JsonResponse;
 
+use Illuminate\Support\Facades\Log;
+
 class UpdateFormController
 {
     public function __invoke(UpdateFormRequest $request, UpdateFormAction $updateFormAction): JsonResponse
     {
+        Log::info(
+            'UpdateFormController####################################################################################################################################################################'
+        );
+        Log::info('===UpdateFormController=====> Request data: ', $request->all());
+        Log::info('UpdateFormController-user_id: ' . $request->input('user_id'));
+        Log::info('UpdateFormController-public_code: ' . $request->input('public_code'));
+        Log::info('UpdateFormController-id: ' . $request->input('id'));
+
         sleep(1);
         $formIdToUpdate = (string) $request->id;
-
         $user_id = $request->input('user_id');
         $public_code = $request->input('public_code');
 
         $form = Form::find($formIdToUpdate);
+        Log::info('UpdateFormController-form-id: ' . $form->id);
+        Log::info('UpdateFormController-form-user_id: ' . $form->user_id);
+        Log::info('UpdateFormController-form-public_code: ' . $form->public_code);
+
         if (
-            ! $form
+            !$form
             || (string) $form->user_id !== (string) $user_id
             || (string) $form->public_code !== (string) $public_code
         ) {
