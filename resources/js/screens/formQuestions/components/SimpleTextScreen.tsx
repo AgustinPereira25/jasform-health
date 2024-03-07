62980e976136ffb15aca6d2cd74003493f351794
Agustín Pereira <a.pereira.clavijo.25@gmail.com>
2024-03-07 18:26:49 -0300

    mapping key

--- resources/js/screens/formQuestions/components/SimpleTextScreen.tsx
+++ resources/js/screens/formQuestions/components/SimpleTextScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Input } from '@/ui'
 import type { Question } from '@/api';
@@ -15,38 +15,32 @@ export const SimpleTextScreen: React.FC<SimpleTextScreenProps> = ({ currentQuest
     // console.log(formQuestions);
     const [title, setTitle] = React.useState(currentQuestion.title ?? '');
     const [textToShow, setTextToShow] = React.useState(currentQuestion.text ?? '');
-    // const [mappingKey, setMappingKey] = useState(currentQuestion.mapping_key ?? '');
+    const [mappingKey, setMappingKey] = useState(currentQuestion.mapping_key ?? '');
 
     useEffect(() => {
         setTitle(currentQuestion.title ?? '');
         setTextToShow(currentQuestion.text ?? '');
     }, [currentQuestion.title, currentQuestion.text]);
 
-    // useEffect(() => {
-    //     setTitle(currentQuestion.title ?? '');
-    //     setTextToShow(currentQuestion.text ?? '');
-    //     setMappingKey(currentQuestion.mapping_key ?? '');
-    // }, [currentQuestion.title, currentQuestion.text, currentQuestion.mapping_key]);
+    useEffect(() => {
+        setTitle(currentQuestion.title ?? '');
+        setTextToShow(currentQuestion.text ?? '');
+        setMappingKey(currentQuestion.mapping_key ?? '');
+    }, [currentQuestion.title, currentQuestion.text, currentQuestion.mapping_key]);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { id, value } = event.target;
-        // console.log(id, value)
-        if (id === 'title') {
-            setTitle(value);
-        } else if (id === 'text') {
-            setTextToShow(value);
+        switch (id) {
+            case 'title':
+                setTitle(value);
+                break;
+            case 'text':
+                setTextToShow(value);
+                break;
+            case 'mapping_key':
+                setMappingKey(value);
+                break;
         }
-        // switch (id) {
-        //     case 'title':
-        //         setQuestionToShow(value);
-        //         break;
-        //     case 'text':
-        //         setTextToShow(value);
-        //         break;
-        //     case 'mapping_key':
-        //         setMappingKey(value);
-        //         break;
-        // }
         // Update the formQuestions general state
         const updatedQuestions = formQuestions?.map((question) => {
             if (question.order === currentQuestion.order) {
@@ -92,7 +86,7 @@ export const SimpleTextScreen: React.FC<SimpleTextScreenProps> = ({ currentQuest
                 />
             </div>
             <hr />
-            {/* <div className="flex flex-col gap-3 md:flex-row py-4">
+            <div className="flex flex-col gap-3 md:flex-row py-4">
                 <span className="shrink-0 w-28">Mapping key</span>
                 <Input
                     containerClassName="w-full"
@@ -104,7 +98,7 @@ export const SimpleTextScreen: React.FC<SimpleTextScreenProps> = ({ currentQuest
                     onChange={(event) => handleChange(event)}
                 />
             </div>
-            <hr /> */}
+            <hr />
         </div>
     )
 }
