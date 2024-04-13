22531a8bee07685914c8778e730ef4cd5eef03e3
joasegovia9427 <joasegovia9427@gmail.com>
2024-04-13 15:46:54 -0300

    Create User without session

--- resources/js/screens/login/Register.tsx
+++ resources/js/screens/login/Register.tsx
@@ -6,14 +6,17 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
+import { Tooltip } from "flowbite-react";
 
-import { recoverMutation } from "./loginAuth";
+import type { RegisterParams } from "./loginAuth";
+import { registerMutation } from "./loginAuth";
 import { Button, Input, LoadingOverlay } from "@/ui";
 import HomeTextAndImage from "@/components/HomeTextAndImage";
 import { ROUTES } from "@/router";
 import { useUserStore } from "@/stores";
+import type { IHttpResponseError } from "@/api";
 
-const recoverSchema = z
+const registerSchema = z
     .object({
         firstName: z
             .string()
@@ -23,6 +26,10 @@ const recoverSchema = z
             .string()
             .trim()
             .min(1, { message: "Please enter last name" }),
+        organization: z
+            .string()
+            .trim()
+            .min(1, { message: "Please enter your organization" }),
         email: z
             .string()
             .min(1, { message: "Email is required" })
@@ -30,9 +37,13 @@ const recoverSchema = z
         password: z
             .string()
             .trim()
-            .min(1, { message: "Please enter Password" }),
+            .min(8, { message: "Password needs at least 8 characters" })
+            .refine(
+                password => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password),
+                { message: "Password dose not meet the rules" }
+            ),
     });
-type RecoverFormValues = z.infer<typeof recoverSchema>;
+type RegisterFormValues = z.infer<typeof registerSchema>;
 
 export const Register = () => {
     const navigate = useNavigate();
@@ -47,31 +58,56 @@ export const Register = () => {
         formState: { errors },
         handleSubmit,
         register,
-    } = useForm<RecoverFormValues>({
-        resolver: zodResolver(recoverSchema),
+    } = useForm<RegisterFormValues>({
+        resolver: zodResolver(registerSchema),
     });
 
-    const { mutate: recoverUserMutation, isPending: isPendingRecoverUserMutation } =
+    const { mutate: registerUserMutation, isPending: isPendingRegisterUserMutation } =
         useMutation({
-            mutationFn: recoverMutation.mutation,
-            onError: (error: any) => {
-                if (error.response) {
-                    toast.error(error.response.data.message);
-                } else if (error.request) {
-                    toast.error('Request was made but no response was received');
+            mutationFn: registerMutation.mutation,
+            onSuccess: () => {
+                toast.success(`Your user was successfully created! Try Login now`);
+                navigate(ROUTES.login);
+            },
+            onError: (err: IHttpResponseError) => {
+                if (err.message === "Request failed with status code 404") {
+                    toast.error("There was an error trying to create the user. Please try again later.");
                 } else {
-                    toast.error('Error', error.message);
+                    if (err?.response?.data?.message) {
+                        toast.error(err?.response.data.message);
+                    } else if (err?.response?.data?.error) {
+                        const error = err?.response?.data?.error;
+                        if (typeof error === 'string') {
+                            toast.error(error);
+                        } else if (error?.fields) {
+                            Object.entries(error.fields).forEach(([_, valArray]) => {
+                                toast.error(`${valArray[0]}`);
+                            });
+                        }
+                    } else {
+                        toast.error("There was an error trying to create the user. Please try again later.");
+                    }
                 }
             },
         });
 
-    const onSubmit = (data: RecoverFormValues) => {
-        recoverUserMutation(data);
+    const onSubmit = (data: RegisterFormValues) => {
+        const user_RegisterParams: RegisterParams = {
+            first_name: data.firstName,
+            last_name: data.lastName,
+            organization_name: data.organization,
+            email: data.email,
+            password: data.password,
+            password_confirmation: data.password,
+            role_name: "Creator",
+            is_active: 1
+        }
+        registerUserMutation(user_RegisterParams);
     };
 
     return (
         <>
-            {(isPendingRecoverUserMutation) && (
+            {(isPendingRegisterUserMutation) && (
                 <LoadingOverlay />
             )}
             <div
@@ -89,8 +125,10 @@ export const Register = () => {
                                     id="firstName"
                                     label="First Name"
                                     placeholder="Your name"
+                                    containerClassName="mb-[-20px]"
                                     {...register("firstName")}
                                     error={errors.firstName?.message}
+                                    autoComplete="new-password"
                                 />
                             </div>
                             <div>
@@ -98,8 +136,10 @@ export const Register = () => {
                                     id="lastName"
                                     label="Last Name"
                                     placeholder="Your last name"
+                                    containerClassName="mb-[-20px]"
                                     {...register("lastName")}
                                     error={errors.lastName?.message}
+                                    autoComplete="new-password"
                                 />
                             </div>
                             <div>
@@ -107,6 +147,7 @@ export const Register = () => {
                                     id="email"
                                     label="Email"
                                     placeholder="email@email.com"
+                                    containerClassName="mb-[-20px]"
                                     {...register("email")}
                                     error={errors.email?.message}
                                     autoComplete="new-password"
@@ -114,14 +155,27 @@ export const Register = () => {
                             </div>
                             <div>
                                 <Input
-                                    type="password"
-                                    id="password"
-                                    label="Password"
-                                    placeholder="Enter Password"
-                                    {...register("password")}
-                                    error={errors.password?.message}
+                                    id="organization"
+                                    label="Organization"
+                                    placeholder="Your Organization"
+                                    containerClassName="mb-[-20px]"
+                                    {...register("organization")}
+                                    error={errors.organization?.message}
                                     autoComplete="new-password"
                                 />
+                            </div>
+                            <div>
+                                <Tooltip placement="left" content={"Password must contain at least 8 characters, one uppercase letter, one lowercase letter and one number"} className="text-nowrap w-64">
+                                    <Input
+                                        type="password"
+                                        id="password"
+                                        label="Password"
+                                        placeholder="Enter Password"
+                                        {...register("password")}
+                                        error={errors.password?.message}
+                                        autoComplete="new-password"
+                                    />
+                                </Tooltip>
                             </div>
                             <div className="pb-8">
                                 <Button
@@ -141,9 +195,9 @@ export const Register = () => {
                         <span className="text-sm">Already have a JASForm Account? </span>
                         <button onClick={() => navigate(ROUTES.login)} className="font-semibold text-blue-600 text-sm">Log in</button>
                     </div>
-                </div>
+                </div >
 
-            </div>
+            </div >
         </>
     );
 };
